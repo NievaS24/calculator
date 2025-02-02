f4b0c5efe9c74eabc31cc079ef0e0226731dcb24
NievaS24 <santinieva24@gmail.com>
2025-02-02 02:15:44 -0300

    feat: :sparkles: Boton DEL e ingreso por teclado.
    Se añadio un boton que borra el ultimo input y ahora se puede ingresar valores con teclado.
    Falta mejorar

--- script.js
+++ script.js
@@ -39,38 +39,35 @@ function operate (op, num1, num2) {
 }
 
 function populateDisplay (num) {
-    if (display.textContent == "ERROR") display.textContent = "";
+    if (display.textContent === "ERROR" || display.textContent === "Go Back to School") display.textContent = "";
     display.textContent += num;
 }
 
-let result = 0;
-const display = document.querySelector("#display");
-const btns = document.querySelectorAll(".number");
-const operator = document.querySelectorAll(".operator");
-const equal = document.querySelector("#equal");
-const clear = document.querySelector("#clear");
-
-btns.forEach(btn => btn.addEventListener("click", () => populateDisplay(btn.textContent)));
-
-operator.forEach(btn => btn.addEventListener("click", () => {
-    op = btn.textContent;
-    firstNum = parseFloat(display.textContent);
-    display.textContent = "";
-}));
-
-clear.addEventListener("click", () => {
+function errase () {
+    let array = display.textContent.split("");
+    array.pop();
+    display.textContent = array.join("");
+}
+function clearAll () {
     display.textContent = "";
     firstNum = null;
     op = null;
     secondNum = null;
     result = null
-});
-equal.addEventListener("click", () => {
+}
+function setOperator (btn) {
+    op = btn;
+    if (display.textContent != "") firstNum = parseFloat(display.textContent);
+    display.textContent = "";
+}
+function getResult () {
     secondNum = parseFloat(display.textContent);
     if (op != null && firstNum != null && secondNum != null){
         result = operate(op, firstNum, secondNum);
         if (isNaN(result)) {
             display.textContent = "ERROR";
+        } else if (result == Infinity) {
+            display.textContent = "Go Back to School";
         } else {
             if (Number.isInteger(result)) {
                 display.textContent = result;
@@ -81,4 +78,30 @@ equal.addEventListener("click", () => {
     } else {
         display.textContent = "ERROR";
     }
+}
+
+let result = 0;
+const display = document.querySelector("#display");
+const btns = document.querySelectorAll(".number");
+const operator = document.querySelectorAll(".operator");
+const equal = document.querySelector("#equal");
+const clear = document.querySelector("#clear");
+const backspace = document.querySelector("#backspace");
+
+btns.forEach(btn => btn.addEventListener("click", () => populateDisplay(btn.textContent)));
+
+operator.forEach(btn => btn.addEventListener("click", () => setOperator(btn.textContent)));
+
+clear.addEventListener("click", clearAll);
+
+equal.addEventListener("click", getResult);
+
+backspace.addEventListener("click", errase);
+
+window.addEventListener("keydown", (e) => {
+    if (e.key >= 0 && e.key <= 9) populateDisplay(e.key);
+    if (e.key === "Backspace") errase();
+    if (e.key === "Escape" || e.key === "Delete") clearAll();
+    if (e.key === "+" ||e.key === "-" || e.key === "*" || e.key === "/") setOperator(e.key);
+    if (e.key === "Enter" || e.key === "=") getResult();
 });

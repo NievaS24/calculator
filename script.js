95902512fdac4cbe543ef89547ab4f607fd1991c
NievaS24 <santinieva24@gmail.com>
2025-02-01 21:14:13 -0300

    feat: :sparkles: Funcion operate creada.
    Esta funcion llama a todas las operaciones basicas<br/> Se crearon tres variables que se usan en una operacion.

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b;
 }
+
+let firstNum, operator, secondNumber;
+
+function operate (op, num1, num2) {
+    let result
+    switch (op) {
+        case "+":
+            result = add(num1, num2);
+            break;
+    
+        case "-":
+            result = subtract(num1, num2);
+            break;
+    
+        case "*":
+            result = multiply(num1, num2);
+            break;
+    
+        case "/":
+            result = divide(num1, num2);
+            break;
+    }
+    return result;
+}

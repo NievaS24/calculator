3e73e20b1976c3d6eee6deebe0a254db81aa310f
NievaS24 <santinieva24@gmail.com>
2025-02-02 01:20:38 -0300

    feat: :sparkles: Calculadora funcional 1.0
    La calculadora ya funciona.
     Aunque hay que perfeccionar algunas cosas.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function divide (a, b) {
     return a / b;
 }
 
-let firstNum, operator, secondNumber;
+let firstNum, op, secondNum;
 
 function operate (op, num1, num2) {
     let result
@@ -37,3 +37,48 @@ function operate (op, num1, num2) {
     }
     return result;
 }
+
+function populateDisplay (num) {
+    if (display.textContent == "ERROR") display.textContent = "";
+    display.textContent += num;
+}
+
+let result = 0;
+const display = document.querySelector("#display");
+const btns = document.querySelectorAll(".number");
+const operator = document.querySelectorAll(".operator");
+const equal = document.querySelector("#equal");
+const clear = document.querySelector("#clear");
+
+btns.forEach(btn => btn.addEventListener("click", () => populateDisplay(btn.textContent)));
+
+operator.forEach(btn => btn.addEventListener("click", () => {
+    op = btn.textContent;
+    firstNum = parseFloat(display.textContent);
+    display.textContent = "";
+}));
+
+clear.addEventListener("click", () => {
+    display.textContent = "";
+    firstNum = null;
+    op = null;
+    secondNum = null;
+    result = null
+});
+equal.addEventListener("click", () => {
+    secondNum = parseFloat(display.textContent);
+    if (op != null && firstNum != null && secondNum != null){
+        result = operate(op, firstNum, secondNum);
+        if (isNaN(result)) {
+            display.textContent = "ERROR";
+        } else {
+            if (Number.isInteger(result)) {
+                display.textContent = result;
+            } else {
+                display.textContent = result.toFixed(2);
+            }
+        }
+    } else {
+        display.textContent = "ERROR";
+    }
+});
